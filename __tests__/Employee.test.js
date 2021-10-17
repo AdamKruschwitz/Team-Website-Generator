895916b0ffc2330f19dd62766c20903ec42a7562
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-16 17:22:14 -0700

    fixed employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -15,29 +15,37 @@ describe("Employee", () => {
     });
 
     describe("getName", () => {
-        const name = "Will Smith";
-        const employee = new Employee(name, 1, "w.smith@gmail.com");
+        it("should return the given name", () => {
+            const name = "Will Smith";
+            const employee = new Employee(name, 1, "w.smith@gmail.com");
 
-        expect(employee.getName()).toEqual(name);
+            expect(employee.getName()).toEqual(name);
+        });
     });
 
     describe("getId", () => {
-        const id = 69;
-        const employee = new Employee("Big Chungus", id, "turnedHimselfIntoAPickle@gmail.com");
+        it("should return the given id", () => {
+            const id = 69;
+            const employee = new Employee("Big Chungus", id, "turnedHimselfIntoAPickle@gmail.com");
 
-        expect(employee.getId()).toEqual(id);
+            expect(employee.getId()).toEqual(id);
+        });
     });
 
     describe("getEmail", () => {
-        const email = "s.e.baka@gmail.com";
-        const employee = new Employee("Sus E. Baka", 420, email);
+        it("should return the given email", () => {
+            const email = "s.e.baka@gmail.com";
+            const employee = new Employee("Sus E. Baka", 420, email);
 
-        expect(employee.getEmail()).toEqual(email);
+            expect(employee.getEmail()).toEqual(email);
+        });
     });
 
     describe("getRole", () => {
-        const employee = new Employee("Shelbert Nelburshnelson", 1, "shelbert.n@gmail.com");
+        it("should return 'Employee'", () => {
+            const employee = new Employee("Shelbert Nelburshnelson", 1, "shelbert.n@gmail.com");
 
-        expect(employee.getRole()).toEqual("Employee");
+            expect(employee.getRole()).toEqual("Employee");
+        });
     });
 });
