d37bf94e245ecf42ea098dc35c0c0fe83c6204b7
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-16 17:07:10 -0700

    Employee constructor test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,16 @@
+const Employee = require("../lib/Employee.js");
+
+describe("Employee", () => {
+    describe("Constructor", () => {
+        it("should create a new employee with the given parameters", () => {
+            const name = "John D. Employedman";
+            const id = 1;
+            const email = "j.d.employedman@gmail.com";
+            const employee = new Employee(name, id, email);
+            
+            expect(employee.name).toEqual(name);
+            expect(employee.id).toEqual(id);
+            expect(employee.email).toEqual(email);
+        });
+    });
+});
