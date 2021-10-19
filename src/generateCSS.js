fc16704909ec493922ed56e5c16b5e8c83191a8e
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-18 19:17:48 -0700

    implemented generateCSS

--- src/generateCSS.js
+++ src/generateCSS.js
@@ -1,6 +1,11 @@
 function generateCSS() {
-    // TODO
-    return "";
+    return `.bg-red {
+    background-color: #e84756;
+}
+
+.bg-grey {
+    background-color: #f6f7f9;
+}`;
 }
 
 module.exports = generateCSS;
